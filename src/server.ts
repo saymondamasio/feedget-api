3203e81e3e1916ecdfa7186aa7ec4051ed8a7534
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-06 19:17:35 -0300

    chore: add port variable

--- src/server.ts
+++ src/server.ts
@@ -10,4 +10,6 @@ app.use(express.json())
 app.use(cors({}))
 app.use(routes)
 
-app.listen(3333, () => console.log('Server started on port 3333'))
+app.listen(process.env.port || 3333, () =>
+  console.log('Server started on port 3333')
+)
