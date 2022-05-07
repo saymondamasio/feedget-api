e4d920c6e6850e13ba1f58b8ed15172a590e2548
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-06 21:35:08 -0300

    chore: add port variable

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import { routes } from './routes'
 
 const app = express()
 
+const PORT = process.env.PORT || 3333
+
 app.use(express.json())
 
 app.use(cors({}))
 app.use(routes)
 
-app.listen(process.env.port || 3333, () =>
-  console.log('Server started on port 3333')
-)
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
